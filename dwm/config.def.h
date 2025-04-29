0b75f2d34494992c400b265685aa94d30f0790e9
Pixel2175 <147437230+Pixel2175@users.noreply.github.com>
2025-04-29 17:06:24 +0100

    root

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -64,7 +64,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbordercolor, "-sf", selfgcolor, NULL };
-static const char *termcmd[] = {"bash", "-c", "st -e bash  &  wal -R -q ",NULL};
+static const char *termcmd[] = {"bash", "-c", "st -e bash  &  walrs -R -q ",NULL};
 
 #include "movestack.c"
 static const Key keys[] = {
